d1a7ae017e93b5e1ae1dcaa2aa91716b55371fa3
nashwaff <nashwa.ff2006@gmail.com>
2025-03-21 23:49:22 +0700

    Update

--- main.c
+++ main.c
@@ -1,51 +1,104 @@
 #include "wafi.h"
 #include "farell.h"
 #include "nashwa.h"
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    const int screenWidth = 300;
-    const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "Tetris with Raylib");
+    const int screenWidth = 550;
+    const int screenHeight = 650;
+    InitWindow(screenWidth, screenHeight, "Tetris Kelompok 1A");
+    srand(time(NULL));
+    
+    // Load Assets
+    Texture2D background = LoadTexture("Assets/Background.png");
+    Texture2D frameTexture = LoadTexture("Assets/Background2.png");
+    Texture2D papanScore = LoadTexture("Assets/Papanscore.png");
+    
+    // Audio Setup
+    InitAudioDevice();
+    Music music = LoadMusicStream("Assets/Backsound_Korobeiniki_SonyaBelousova.wav"); 
+    PlayMusicStream(music);
+    SetMusicVolume(music, 1.0f);
+    Sound clearSound = LoadSound("Assets/sound_blokhancur.wav");
+    SetSoundVolume(clearSound, 1.0f);
 
+    // Inisialisasi game
     Grid grid;
     Grid_Init(&grid);
 
     Block currentBlock;
+    Block nextBlocks[3];
     Block_Init(&currentBlock);
-
+    for (int i = 0; i < 3; i++) {
+        Block_Init(&nextBlocks[i]);
+    }
+    
     SetTargetFPS(60);
 
     int gravityCounter = 0;
-    const int gravityDelay = 30; // Blok akan turun setiap 30 frame (0.5 detik jika FPS = 60)
+    const int gravityDelay = 30;
+    int score = 0;
+    int combo = 0;
 
     while (!WindowShouldClose()) {
+        UpdateMusicStream(music);
         gravityCounter++;
         if (gravityCounter >= gravityDelay) {
             gravityCounter = 0;
-            moveDown(&currentBlock, &grid); // Gerakkan blok ke bawah secara otomatis
+            moveDown(&currentBlock, &grid);
         }
 
-        // Handle input
-        if (IsKeyPressed(KEY_LEFT)) moveLeft(&currentBlock, &grid); // Geser kiri
-        if (IsKeyPressed(KEY_RIGHT)) moveRight(&currentBlock, &grid); // Geser kanan
-        if (IsKeyPressed(KEY_UP)) rotateBlock(&currentBlock, &grid); // Rotasi blok
+        if (IsKeyPressed(KEY_LEFT)) moveLeft(&currentBlock, &grid); 
+        if (IsKeyPressed(KEY_RIGHT)) moveRight(&currentBlock, &grid);
+        if (IsKeyPressed(KEY_UP)) rotateBlock(&currentBlock, &grid); 
+        if (IsKeyPressed(KEY_DOWN)) moveDown(&currentBlock, &grid);
+        if (IsKeyPressed(KEY_SPACE)) skipBawah(&currentBlock, &grid);
 
-        // Update game state
         if (CheckCollision(&currentBlock, &grid, 0, 1)) {
             PlaceTetromino(&currentBlock, &grid);
-            int score = ClearRows(&grid);
-            Block_Init(&currentBlock); // Spawn blok baru
-            if (CheckGameOver(&grid)) break;
+            int rowCleared = ClearRows(&grid, &combo);
+            score += rowCleared;
+            if (rowCleared > 0) {
+                PlaySound(clearSound);
+            }
+            currentBlock = nextBlocks[0];
+            for (int i = 0; i < 2; i++) {
+                nextBlocks[i] = nextBlocks[i + 1];
+            }
+            Block_Init(&nextBlocks[2]);
+            
+            if (CheckGameOver(&grid)) {
+                bool retry = HandleGameOver();
+                if (retry) {
+                    ResetGame(&grid, &currentBlock, nextBlocks, &score, &combo);
+                    continue;
+                } else {
+                    break;
+                }
+            }            
         }
 
-        // Render
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            Grid_Draw(&grid);
-            Block_Draw(&currentBlock, 50, 50); // Contoh offset
+        ClearBackground(RAYWHITE);
+        DrawTexture(background, 360, 0, WHITE);
+        DrawTexture(frameTexture, 0, 0, WHITE);
+        DrawTexture(papanScore, 365, 100, WHITE);
+        Grid_Draw(&grid, 30, 30);
+        Block_Draw(&currentBlock, 30, 30);
+        DrawNextBlocks(nextBlocks, 320, 290);
+        DrawText("SCORE", 390, 50, 40, WHITE);
+        DrawText(TextFormat("%d", score), 440, 110, 30, WHITE);
+        DrawText("Next Block", 375, 200, 30, WHITE);
         EndDrawing();
     }
 
+    UnloadSound(clearSound);
+    UnloadMusicStream(music);
+    CloseAudioDevice();
+    UnloadTexture(frameTexture);
+    UnloadTexture(background);
+    UnloadTexture(papanScore);
     CloseWindow();
     return 0;
 }
--- nashwa.c
+++ nashwa.c
@@ -1,4 +1,6 @@
 #include "nashwa.h"
+#include <stdlib.h>
+#include <raylib.h>
 
 bool CheckCollision(Block *block, Grid *grid, int dx, int dy) {
     for (int i = 0; i < 4; i++) {
@@ -16,7 +18,7 @@ void PlaceTetromino(Block *block, Grid *grid) {
         int row = block->cells[block->rotationState][i].row;
         int col = block->cells[block->rotationState][i].column;
         if (row >= 0 && row < NUM_ROWS && col >= 0 && col < NUM_COLS) {
-            grid->grid[row][col] = block->id + 1; // Simpan ID blok ke grid
+            grid->grid[row][col] = block->id + 1;
         }
     }
 }
@@ -28,8 +30,8 @@ bool CheckGameOver(Grid *grid) {
     return false;
 }
 
-int ClearRows(Grid *grid) {
-    int score = 0;
+int ClearRows(Grid *grid, int *combo) {
+    int rowsCleared = 0;
     for (int row = NUM_ROWS - 1; row >= 0; row--) {
         bool full = true;
         for (int col = 0; col < NUM_COLS; col++) {
@@ -39,7 +41,7 @@ int ClearRows(Grid *grid) {
             }
         }
         if (full) {
-            score += 100;
+            rowsCleared++;
             for (int r = row; r > 0; r--) {
                 for (int col = 0; col < NUM_COLS; col++) {
                     grid->grid[r][col] = grid->grid[r - 1][col];
@@ -51,5 +53,67 @@ int ClearRows(Grid *grid) {
             row++;
         }
     }
-    return score;
+    return ApplyComboScore(rowsCleared, combo);
+}
+
+int ApplyComboScore(int rowsCleared, int *combo) {
+    if (rowsCleared == 5) {
+        int bonus = (*combo) * 100;  
+        (*combo)++; 
+        return 500 + bonus;
+    } else if (rowsCleared > 0) {
+        *combo = 1;  
+        return rowsCleared * 100;
+    }
+    return 0;
+}
+
+void ResetGame(Grid *grid, Block *currentBlock, Block *nextBlocks, int *score, int *combo) {
+    Grid_Init(grid);
+    Block_Init(currentBlock);
+    for (int i = 0; i < 3; i++) {
+        Block_Init(&nextBlocks[i]);
+    }
+    *score = 0;
+    *combo = 0;
+}
+
+bool HandleGameOver() {
+    int screenWidth = 480;
+    int screenHeight = 640;
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+  
+        int textGameOverX = (screenWidth - MeasureText("GAME OVER", 40)) / 2 + 30;
+        int textGameOverY = 200;  // Bisa atur sedikit kalau kurang pas
+        DrawText("GAME OVER", textGameOverX, textGameOverY, 40, RED);
+
+        int buttonWidth = 220, buttonHeight = 50;
+        int buttonX = (screenWidth - buttonWidth) / 2 + 30; 
+        int textRetryX = buttonX + (buttonWidth - MeasureText("Retry", 25)) / 2;  
+        int textExitX = buttonX + (buttonWidth - MeasureText("Exit", 25)) / 2;
+
+        DrawRectangle(buttonX, 300, buttonWidth, buttonHeight, DARKGRAY);
+        DrawText("Retry", textRetryX, 315, 25, WHITE);
+
+        DrawRectangle(buttonX, 370, buttonWidth, buttonHeight, DARKGRAY);
+        DrawText("Exit", textExitX, 385, 25, WHITE);
+
+        EndDrawing();
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            Vector2 mouse = GetMousePosition();
+            if (mouse.x >= buttonX && mouse.x <= buttonX + buttonWidth) {
+                if (mouse.y >= 300 && mouse.y <= 350) {
+                    return true;
+                }
+                if (mouse.y >= 370 && mouse.y <= 420) {
+                    return false;
+                }
+            }
+        }
+    }
+    return false;
 }
