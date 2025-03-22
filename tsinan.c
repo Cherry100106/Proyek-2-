1b9103a83bd8435c3df5f44a7c42789067c818b5
TsinanAJST <tsinan.arkan.tif24@polban.ac.id>
2025-03-22 23:26:56 +0700

    terbaru

--- tsinan.c
+++ tsinan.c
@@ -1,11 +1,15 @@
 #include "tsinan.h"
 #include <stdio.h>
+#include <raylib.h> // Pastikan raylib disertakan jika menggunakan DrawText
+
 
 // Menampilkan Highscore
 void loadHighScore(int *highScore) {
     FILE *file = fopen(HIGHSCORE_FILE, "r");
     if (file) {
-        fscanf(file, "%d", highScore);
+        if (fscanf(file, "%d", highScore) != 1) {
+            *highScore = 0; // Jika gagal membaca, set default 0
+        }
         fclose(file);
     } else {
         *highScore = 0; // Default high score jika file tidak ditemukan
@@ -16,12 +20,14 @@ void loadHighScore(int *highScore) {
 void saveHighScore(int score) {
     int highScore;
     loadHighScore(&highScore);
-    
+
     if (score > highScore) {
         FILE *file = fopen(HIGHSCORE_FILE, "w");
         if (file) {
             fprintf(file, "%d", score);
             fclose(file);
+        } else {
+            printf("Gagal menyimpan high score!\n");
         }
     }
 }
@@ -33,7 +39,7 @@ int getHighScore() {
     return highScore;
 }
 
-// Display highscore
+// Display highscore menggunakan Raylib
 void displayHighScore(int offsetX, int offsetY) {
     int highScore = getHighScore();
     DrawText(TextFormat("High Score: %d", highScore), offsetX, offsetY, 20, WHITE);
