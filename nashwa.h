c1056f4b2f063071c73cb630d1ac1e132724cc58
Farell Daffa Aditya <farelldaffaaditya100106@gmail.com>
2025-03-22 14:54:12 +0700

    update merge dari branch nashwa

--- nashwa.h
+++ nashwa.h
@@ -1,11 +1,15 @@
 #ifndef NASHWA_H
 #define NASHWA_H
 
-#include "wafi.h" 
+#include "wafi.h"
+#include <stdbool.h>
 
 bool CheckCollision(Block *block, Grid *grid, int dx, int dy);
 void PlaceTetromino(Block *block, Grid *grid);
 bool CheckGameOver(Grid *grid);
-int ClearRows(Grid *grid);
+int ClearRows(Grid *grid, int *combo);
+int ApplyComboScore(int rowsCleared, int *combo);
+void ResetGame(Grid *grid, Block *currentBlock, Block *nextBlocks, int *score, int *combo);
+bool HandleGameOver();
 
 #endif
