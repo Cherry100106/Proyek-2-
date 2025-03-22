439eef3063d7f2426c385aa9d6aed049a34a3e2d
Farell Daffa Aditya <farelldaffaaditya100106@gmail.com>
2025-03-22 20:00:07 +0700

    Merge dari master ke branch farell

--- farell.h
+++ farell.h
@@ -1,12 +1,13 @@
 #ifndef FARELL_H
 #define FARELL_H
-
 #include "wafi.h"
+
 void moveLeft(Block *block, Grid *grid);
 void moveRight(Block *block, Grid *grid);
 void moveDown(Block *block, Grid *grid);
 void rotateBlock(Block *block, Grid *grid);
 void skipBawah(Block *Block, Grid *grid);
 void DrawNextBlocks(Block nextBlocks[], int offsetX, int offsetY);
+void DrawGhostPiece(Block *block, Grid *grid, int offsetX, int offsetY);
 
 #endif
